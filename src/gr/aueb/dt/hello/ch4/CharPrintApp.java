3bd2b15282b2315f4a97e80a86a70d7b1c3cde4e
a8anassis <a8anassis@gmail.com>
2025-10-18 10:14:29 +0300

    feat: add char data type example

---
+++ src/gr/aueb/dt/hello/ch4/CharPrintApp.java
@@ -0,0 +1,15 @@
+package gr.aueb.dt.hello.ch4;
+
+/**
+ * A simple demo of char data type
+ */
+public class CharPrintApp {
+
+    public static void main(String[] args) {
+        char ch1 = 's';
+        char ch2 = '8';
+
+        System.out.printf("char: %c, ordinal: %d\n", ch1, (int) ch1);
+        System.out.printf("char: %c, ordinal: %d\n", ch2, (int) ch2);
+    }
+}
