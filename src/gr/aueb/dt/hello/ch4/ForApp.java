7d18baa410f588bdb55e7c143f1cd70d6e7badd5
a8anassis <a8anassis@gmail.com>
2025-10-18 10:15:22 +0300

    feat: add intro example with for

---
+++ src/gr/aueb/dt/hello/ch4/ForApp.java
@@ -0,0 +1,15 @@
+package gr.aueb.dt.hello.ch4;
+
+/**
+ * A simple demo with 'for' control
+ * structure.
+ */
+public class ForApp {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
