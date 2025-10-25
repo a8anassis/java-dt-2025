065bb2fbea3feec4378dc540a0035152baaa4ba6
a8anassis <a8anassis@gmail.com>
2025-10-25 08:54:10 +0300

    feat: add float / double examples

---
+++ src/gr/aueb/dt/hello/ch5/AddApp.java
@@ -0,0 +1,23 @@
+package gr.aueb.dt.hello.ch5;
+
+/**
+ * It demonstrates the sum with a method.
+ */
+public class AddApp {
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int result = 0;
+        int result2 = 0;
+
+        result = add(num1, num2);
+        result2 = add(30, num2);
+
+        System.out.println("Result: " + result);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
