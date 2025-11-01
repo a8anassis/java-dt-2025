72a104c933f7e956cfdcc1c342c96fedc291586f
a8anassis <a8anassis@gmail.com>
2025-11-01 10:33:34 +0200

    fix: clear

--- src/gr/aueb/dt/hello/ch5/AddApp.java
+++ src/gr/aueb/dt/hello/ch5/AddApp.java
@@ -6,10 +6,13 @@ package gr.aueb.dt.hello.ch5;
 public class AddApp {
 
     public static void main(String[] args) {
+
+        // Declaration and initialization
         int num1 = 10;
         int num2 = 20;
         int result = 0;
         int result2 = 0;
+
 
         result = add(num1, num2);
         result2 = add(30, num2);
--- src/gr/aueb/dt/hello/ch5/FactorialApp.java
+++ src/gr/aueb/dt/hello/ch5/FactorialApp.java
@@ -10,16 +10,20 @@ import java.util.Scanner;
 public class FactorialApp {
 
     public static void main(String[] args) {
+
+        // Declaration and initialization
         Scanner scanner = new Scanner(System.in);
         int num = 0;
         int result = 0;
 
+        // Data Insert - Data Binding
         System.out.println("Please enter a number (integer)");
         num = scanner.nextInt();
 
-        // Call the factorial method (service)
+        // Process with the data -- Call the factorial method (service)
         result = facto(num);
 
+        // Display the result to the user -- Feedback
         System.out.println("The factorial of " + num + " is " + result);
     }
 
@@ -30,12 +34,16 @@ public class FactorialApp {
      * @return      the factorial of n.
      */
     public static int facto(int n) {
+
+        // Declaration and initialization
         int result = 1;
 
+        // Process with the data
         for (int i = 1; i <= n; i++) {
             result *= i;    // result = result * i;
         }
 
+        // Return the result
         return result;
     }
 }
