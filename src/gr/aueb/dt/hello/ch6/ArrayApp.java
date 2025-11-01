d319758e077e9dfd39cf3fa551cb4fec56c61d40
a8anassis <a8anassis@gmail.com>
2025-11-01 10:33:49 +0200

    feat: add array logic

---
+++ src/gr/aueb/dt/hello/ch6/ArrayApp.java
@@ -0,0 +1,19 @@
+package gr.aueb.dt.hello.ch6;
+
+/**
+ * Array Demo.
+ */
+public class ArrayApp {
+
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+
+        arr[0] = 12;
+        arr[1] = 25;
+        arr[2] = 61;
+
+        System.out.println("1st element: " + arr[0]);
+        System.out.println("2nd element: " + arr[1]);
+        System.out.println("3rd element: " + arr[2]);
+    }
+}
