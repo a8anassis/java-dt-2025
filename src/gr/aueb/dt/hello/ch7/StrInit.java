e9dee4510d79a57257a51c9b954bb6cd7bd646c3
a8anassis <a8anassis@gmail.com>
2025-11-29 10:56:38 +0200

    feat: add String declarations and immutability

---
+++ src/gr/aueb/dt/hello/ch7/StrInit.java
@@ -0,0 +1,14 @@
+package gr.aueb.dt.hello.ch7;
+
+public class StrInit {
+
+    public static void main(String[] args) {
+        String s1 = "Athens";
+        String s2 = new String("Athens");
+        String s3 = "";
+        String s4 = null;
+        String s5 = "Athens";
+
+
+    }
+}
